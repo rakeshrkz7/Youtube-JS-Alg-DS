d087b8f4b6913dfcb2e96f01a607dc9ea9bc546d
Rakesh <rakeshrkz15798@gmail.com>
2022-05-04 20:44:46 +0530

    chunk solution added

--- chunk/index.js
+++ chunk/index.js
@@ -6,7 +6,24 @@
 // chunk(["a", "b", "c", "d"], 2) --> [[ "a", "b"], ["c", "d"]]
 // chunk([0, 1, 2, 3, 4, 5], 4) -->  [[0, 1, 2, 3], [4, 5]]
 
-function chunk(array, size) {}
+// Time complexity = O(n) 
+// Space complexity = O(n) 
+function chunk(array, size) {
+  let res = [];
+
+  for(let i=0; i<array.length; i++) {
+    const item = array[i];
+    const last = res[res.length - 1];
+
+    if(!last || last.length === size) {
+      res.push([item]);
+    } else {
+      last.push(item);
+    }
+  }
+
+  return res;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
