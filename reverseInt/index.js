e788c0647fb34831da567418cdfae36c9ce5b7e0
Rakesh <rakeshrkz15798@gmail.com>
2022-05-04 13:34:51 +0530

    reverseInt solution added

--- reverseInt/index.js
+++ reverseInt/index.js
@@ -7,7 +7,15 @@
 //   reverseInt(-13) === -31
 //   reverseInt(-100) === -1
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let reversed = (n + '').split('').reverse().join('');
+  
+  reversed = parseInt(reversed);
+  if(n<0)
+    reversed = reversed * -1
+    
+  return reversed;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
